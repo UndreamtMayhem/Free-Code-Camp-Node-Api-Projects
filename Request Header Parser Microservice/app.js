9835ee7eff05e76b9125f4c297c699701185e498
undreammayhem <danny_boyx@hotmail.co.uk>
2017-08-17 19:41:03 +0000

    Request Header Parser Microservice completed

--- Request Header Parser Microservice/app.js
+++ Request Header Parser Microservice/app.js
@@ -9,36 +9,17 @@ const app = express();
 
 
 
-// Load View Engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// Body Parser Middleware
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
-app.use(bodyParser.json());
-
-// Set Public Folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-
 // Home Route
 app.get('/', function(req, res){
   var ip_info = get_ip(req);
-  console.log(ip_info['clientIp']);
-  //res.send(res);
-   res.send('ip: ' + req.connection.remoteAddress);
-   // user agent sorted
-   console.log(req.headers['user-agent'])
-   console.log(req.headers['accept-language']);
-   console.log(req.socket.remoteAddress);
-  //console.log(req);
-});
-// Home Route
-app.get('/:id', function(req, res){
-  //res.send(res);
-   res.send('id: ' + req.params.id);
+  let ip_Add = ip_info['clientIp'];
+
+   let user_agent = req.headers['user-agent'];
+   let language = req.headers['accept-language']
+   //console.log(req.socket.remoteAddress);
+
+   //output
+   res.json({"ipaddress":ip_Add,"language":language,"software":user_agent});
   //console.log(req);
 });
 
