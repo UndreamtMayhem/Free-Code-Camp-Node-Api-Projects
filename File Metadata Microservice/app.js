80500f451d1553d1cfe7f068c8ce864baaaaf80e
undreammayhem <danny_boyx@hotmail.co.uk>
2017-08-17 20:45:49 +0000

    "file uploaded almost finished"

--- File Metadata Microservice/app.js
+++ File Metadata Microservice/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
-let get_ip = require('ipware')().get_ip;
 
+
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
 // Init App
 const app = express();
 
@@ -10,38 +11,22 @@ const app = express();
 
 
 // Load View Engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/views'));
 
-// Body Parser Middleware
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
-app.use(bodyParser.json());
 
-// Set Public Folder
-app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 
 // Home Route
 app.get('/', function(req, res){
-  var ip_info = get_ip(req);
-  console.log(ip_info['clientIp']);
-  //res.send(res);
-   res.send('ip: ' + req.connection.remoteAddress);
-   // user agent sorted
-   console.log(req.headers['user-agent'])
-   console.log(req.headers['accept-language']);
-   console.log(req.socket.remoteAddress);
-  //console.log(req);
-});
-// Home Route
-app.get('/:id', function(req, res){
-  //res.send(res);
-   res.send('id: ' + req.params.id);
-  //console.log(req);
+  res.render("index");
 });
 
+// Home Route
+app.post('/file-size', upload.single('file'), function(req, res){
+    res.json({"file-size": req.file.size});
+  });
 
 // Start Server
 app.listen(3000, function(){
